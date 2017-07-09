8ccf8c69117762d6c1e2aa8ecb2e443ff79b7ba3
Alleen Wang <alleen.wang@advantech.com.tw>
2017-07-09 09:57:34 +0800

    Only enalbe flash_erase.

--- Android.mk
+++ Android.mk
@@ -231,3 +231,22 @@ LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_EXECUTABLE)
 
+# flash_erase util
+#
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := flash_erase.c
+
+LOCAL_CFLAGS = -O2 -Wall
+LOCAL_CFLAGS += -Wall -Wextra -Wwrite-strings -Wno-sign-compare -D_FILE_OFFSET_BITS=64
+
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
+
+LOCAL_STATIC_LIBRARIES := libc
+
+LOCAL_MODULE := flash_erase
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_EXECUTABLE)
+
